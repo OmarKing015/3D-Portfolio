f7e48df116439e979723233f7218730bc19869a1
OmarKing015 <66084757+OmarKing015@users.noreply.github.com>
2023-10-12 23:32:39 +0200

    balls

--- src/components/Tech.jsx
+++ src/components/Tech.jsx
@@ -1,18 +1,25 @@
-import React from 'react'
-import {BallCanvas} from './canvas'
-import {SectionWrapper} from '../hoc'
-import { technologies } from '../constans'
+import React from "react";
+import { BallCanvas } from "./canvas";
+import { SectionWrapper } from "../hoc";
+import { technologies } from "../constans";
+import { motion } from "framer-motion";
+import { textVariant } from "../utils/motion";
+import { styles } from "../style";
 
 const Tech = () => {
   return (
-    <div className='flex flex-row flex-wrap justify-center gap-10'>
-
-      {technologies.map((technoloy,index)=>(
-        <div className='w-28 h-28' key={technoloy.name}><BallCanvas icon={technoloy.icon}/></div>
+    <div className="flex flex-row flex-wrap justify-center gap-10">
+      <motion.div variants={textVariant()}>
+        
+        <h3 className={`${styles.sectionHeadText} text-center `}>Some of Important technologies I use often</h3>
+      </motion.div>
+      {technologies.map((technoloy) => (
+        <div className="w-28 h-28" key={technoloy.name}>
+          <BallCanvas icon={technoloy.icon} />
+        </div>
       ))}
-
     </div>
-  )
-}
+  );
+};
 
-export default SectionWrapper(Tech)
+export default SectionWrapper(Tech);
--- src/constans/index.js
+++ src/constans/index.js
@@ -60,10 +60,10 @@ import {
   ];
   
   const technologies = [
-    {
-      name: "HTML 5",
-      icon: html,
-    },
+    // {
+    //   name: "HTML 5",
+    //   icon: html,
+    // },
     {
       name: "CSS 3",
       icon: css,
